25eea3f18a2010fdee69cb5e8c59e334db42837c
hirorohi <227736546+hirorohi03@users.noreply.github.com>
2026-04-26 13:52:53 +0900

    Reapply "civitai.com to civitai.red"
    This reverts commit 904a29f1ea9bf389639b9414f21a80cebb3c0b37.

--- Download/CivitaiApiKey.bat
+++ Download/CivitaiApiKey.bat
@@ -7,7 +7,7 @@ exit /b 1
 @REM 対応するなら Civitai Helper 用に cofig.json の書き戻しもほしいが、今はそこまででない
 
 echo "Civitai のアカウントを作成して、APIキーを取得して、設定してください。"
-echo "アカウント設定の一番下の方で取得できます。https://civitai.com/user/account Ctrl+クリックで開く"
+echo "アカウント設定の一番下の方で取得できます。https://civitai.red/user/account Ctrl+クリックで開く"
 echo "参考: https://github.com/zixaphir/Stable-Diffusion-Webui-Civitai-Helper/wiki/Civitai-API-Key"
 echo.
 echo "現在の CIVITAI_API_KEY: %CIVITAI_API_KEY%"
